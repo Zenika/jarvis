cc984c45cdff67fef79707397b748432dadc02c4
Germain Potet <germain.potet@zenika.com>
2014-06-26 22:52:21 +0200

    Ajout du numero de commit accompagnant le build number

--- back/src/server.js
+++ back/src/server.js
@@ -17,14 +17,14 @@
 	 * Returns a promise which is going to provide jenkins jobs.
 	 */
 	var getJobs = function() {
-		return $http.get(constants.jenkins.url, constants.jenkins.port, '/api/json');		
+		return $http.get(constants.jenkins.url, constants.jenkins.port, '/api/json?depth=1');
 	}
 
 	/**
 	 * Returns a promise which is going to get the information about a job.
 	 */
 	var getJob = function(jobName) {
-		return $http.get(constants.jenkins.url, constants.jenkins.port, '/job/' + jobName + '/api/json');		
+		return $http.get(constants.jenkins.url, constants.jenkins.port, '/job/' + jobName + '/api/json?depth=1');
 	}
 	// Setting parameters
 	app.set('public', '../../front/src/');
